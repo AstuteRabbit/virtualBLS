10650d3d14d81fe960ff23bbb1413136da624322
AstuteRabbit <josh@astuterabbitmedia.com>
2026-03-25 13:50:39 -0400

    Delete New folder/New folder (2)/New folder (3) directory

--- New folder/New folder (2)/New folder (3)/get-settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if (file_exists('settings.json')) {
-        $json = file_get_contents('settings.json');
-        echo $json;
-    } else {
-        echo json_encode([]);
-    }
-}
