46fc3f9e79400020c0da33dd63848d75d40290ea
AstuteRabbit <josh@astuterabbitmedia.com>
2026-05-04 16:05:53 -0400

    Active Subscription verification in settings.php
    Fixes a bug in the workflow that could have allowed a new user to bypass the subscription and still gain access to the service

--- Templates/settings.php
+++ Templates/settings.php
@@ -5,7 +5,7 @@
 //You should have received a copy of the GNU General Public License along with Virtual BLS. If not, see <https://www.gnu.org/licenses/>
 
 
-      // Make sure the user is coming from WordPress and has a valid session.
+      // Make sure users are coming from WordPress and have a valid session.
 
       if (!session_id()) {
             session_start();
@@ -18,6 +18,33 @@
       if (!$token_is_valid) {
             http_response_code(403);
             die('Access Denied. Please log in to virtualBLS.net to access this page through the provided links.');
+      }
+
+      // Load WordPress so auth + Paid Memberships Pro checks are available.
+      $wp_load_path = $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
+      if (!file_exists($wp_load_path)) {
+            http_response_code(500);
+            die('Configuration error. Please contact support.');
+      }
+
+      require_once $wp_load_path;
+
+      if (!is_user_logged_in()) {
+            http_response_code(403);
+            die('Access Denied. Please log in to virtualBLS.net to continue.');
+      }
+
+      if (!function_exists('pmpro_hasMembershipLevel')) {
+            http_response_code(500);
+            die('Membership check is unavailable. Please contact support.');
+      }
+
+      $current_user_id = get_current_user_id();
+      $has_active_membership = pmpro_hasMembershipLevel(null, $current_user_id);
+
+      if (!$has_active_membership) {
+            http_response_code(403);
+            die('Access Denied. An active membership is required to view this page.');
       }
       ?>
 <!DOCTYPE html>
